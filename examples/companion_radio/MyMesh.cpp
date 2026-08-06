c2826b40088848b39e8e20470bd7055c50b8bf30
Holger Adams <mail@holad.de>
2026-08-06 15:42:39 +0200

    Repeater, companion_radio: gate sleep on Dispatcher::isIdle()

--- examples/companion_radio/MyMesh.cpp
+++ examples/companion_radio/MyMesh.cpp
@@ -2441,5 +2441,5 @@ bool MyMesh::advert() {
 
 // To check if there is pending work
 bool MyMesh::hasPendingWork() const {
-  return _mgr->getOutboundTotal() > 0 || dirty_contacts_expiry != 0;
+  return !isIdle() || dirty_contacts_expiry != 0;
 }
