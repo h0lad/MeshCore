3284ca5386e92f056afb8b97b25854e047abfa77
Holger Adams <mail@holad.de>
2026-09-11 23:12:53 +0200

    powersaving: drop unsupported FEM LNA bits and fix doResetAGC arity

--- examples/simple_room_server/MyMesh.cpp
+++ examples/simple_room_server/MyMesh.cpp
@@ -733,7 +733,6 @@ void MyMesh::begin(FILESYSTEM *fs) {
   radio_driver.setRxBoostedGainMode(_prefs.rx_boosted_gain);
 
   board.attachDynamicPrefs(_prefs.getCustom());
-  board.setLoRaFemLnaEnabled(_prefs.radio_fem_rxgain);
   setRxPowerSaving(_prefs.rx_powersaving_enabled, _prefs.rx_ps_rx_us, _prefs.rx_ps_sleep_us);
 
   updateAdvertTimer();
